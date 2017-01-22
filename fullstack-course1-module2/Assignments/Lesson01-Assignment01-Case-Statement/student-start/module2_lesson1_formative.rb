0a2b7f2ea790634baea258c55d5e8ca0f4d980c6
Jess <jesskiri@gmail.com>
2017-01-22 13:40:49 -0500

    completed Mod2 Assigment 1

--- fullstack-course1-module2/Assignments/Lesson01-Assignment01-Case-Statement/student-start/module2_lesson1_formative.rb
+++ fullstack-course1-module2/Assignments/Lesson01-Assignment01-Case-Statement/student-start/module2_lesson1_formative.rb
@@ -1,15 +1,37 @@
+# some_var = "false"
+# another_var = "nil"
+#
+# if some_var == "pink elephant"
+#   puts "Don't think about the pink elephant!"
+#
+# elsif another_var.nil?
+#   puts "Question mark in the method name?"
+#
+# elsif some_var == false
+#   puts "Looks like this one should execute"
+#
+# else
+#   puts "I guess nothing matched... But why?"
+# end
+
+##
+# The functional goal of the assignment is to rewrite a provided
+# if/else statement using a case statement.
+
+
 some_var = "false"
 another_var = "nil"
 
-if some_var == "pink elephant"
+case
+when some_var == "pink elephant"
   puts "Don't think about the pink elephant!"
-
-elsif another_var.nil?
+  # some_var = "pink elephant"
+when another_var.nil?
   puts "Question mark in the method name?"
-
-elsif some_var == false
+  # another_var = nil
+when some_var == false
   puts "Looks like this one should execute"
-
+  # some_var = false
 else
   puts "I guess nothing matched... But why?"
 end
